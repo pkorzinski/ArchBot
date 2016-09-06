8f5344077a6ad94025fc7531bd5ae113c6557714
Jason Holtkamp <holtkam2@gmail.com>
2016-09-06 18:43:39 -0500

    testing api token

--- src/bot.js
+++ src/bot.js
@@ -8,13 +8,13 @@ const request = require('request')
 
 let bot = slack.rtm.client();
 
-let users = slack.users.list(config('xoxb-75559597040-HRAawmZnEeEhwAK53FXbnfiT'), function(err, res){
-  if (err){
-    console.error(err);
-  } else {
-    console.log(res)
-  }
-})
+// let users = slack.users.list(config('SLACK_TOKEN'), function(err, res){
+//   if (err){
+//     console.error(err);
+//   } else {
+//     console.log(res)
+//   }
+// })
 
 let storedMessagesInMemory = [];
 
@@ -35,10 +35,11 @@ var sendMsg = function(){
 
 bot.message((msg) => {
 
+  console.log(config('SLACK_TOKEN'))
+
   storedMessagesInMemory.push(msg);
 
   if (storedMessagesInMemory.length > 3){
-    console.log(users)
     sendMsg();
   }
 
