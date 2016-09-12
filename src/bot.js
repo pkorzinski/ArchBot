68d04064fa6f9ccb891acbcfcd063c8ba82005ca
Jason Holtkamp <holtkam2@gmail.com>
2016-09-12 14:23:20 -0500

    console log

--- src/bot.js
+++ src/bot.js
@@ -39,7 +39,7 @@ var getPassword = function(msg, callback){
     if (err){
       console.error(err)
     } else {
-      console.log(body.password)
+      console.log(body)
       console.log("getPassword was called!")
       callback(body.password);
     }
