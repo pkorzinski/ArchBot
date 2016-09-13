6d2401fc235d94a1e9075ba10a5797a057e8facb
Jim Yang <jim@sidefra.me>
2016-09-12 22:45:57 -0400

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -31,6 +31,7 @@ var sendMsg = function(){
 
 
 var getPassword = function(msg, callback){
+  console.log('getpassword', msg)
   var obj = JSON.stringify{msg.team}
   request({
     url: 'https://hrr18-doge.herokuapp.com/api/teams/',
